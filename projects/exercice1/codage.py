a27782df82f13528079b5aebd09f0e269d346990
Sablier <sablier@zendikar.fr>
2017-06-02 10:54:09 +0200

    add comments on exercice1

--- projects/exercice1/codage.py
+++ projects/exercice1/codage.py
@@ -5,5 +5,6 @@ def get_letter():
     return ord(random.choice(alphabet))
 
 def creer_chromosome(size):
+    # on crée un nouveau chromosome de la bonne taille sous forme de bytearray
     return bytearray()
     
