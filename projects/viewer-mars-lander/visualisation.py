1870b222793a0da0c93695ed9390ef1052f85d9a
Sablier <sablier@zendikar.fr>
2017-06-02 18:44:24 +0200

    fix path

--- projects/viewer-mars-lander/visualisation.py
+++ projects/viewer-mars-lander/visualisation.py
@@ -4,5 +4,5 @@ import unittest
 class VisualisationTest(unittest.TestCase):
 
     def test_visualisation(self):
-        print("TECHIO> open -s /project/answer/genetic-lander-no-submodule index.html")
+        print("TECHIO> open -s /project/target/genetic-lander-no-submodule index.html")
         
